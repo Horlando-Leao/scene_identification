834aa3d96d76b2df7853dc6e194ca937efad7eb2
Horlando <horlandoleao8@gmail.com>
2021-05-16 16:39:19 -0300

    Ajuste de deploy no heroku

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ from flask import Flask, json, request
 
 app = Flask(__name__)
 
-cors = CORS(app, resource={r"/*": {"origins": "*"}})
+#cors = CORS(app, resource={r"/*": {"origins": "*"}})
 
 from routes.Routes import *
